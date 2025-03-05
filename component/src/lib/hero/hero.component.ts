df74511814be3bb174046dd5105235fb0283a7ff
brettaio <dev@bretta.io>
2025-03-05 15:35:23 -0500

    opps added image size to hero and broke the ssizing

--- component/src/lib/hero/hero.component.ts
+++ component/src/lib/hero/hero.component.ts
@@ -7,12 +7,10 @@ import { appDataStore } from '../../../../websites/enzo-concrete/src/app/store/a
   imports: [CtaButtonAltComponent],
   template: `
     <section
-      class="relative bg-cover bg-center bg-no-repeat"
+      class="relative bg-cover bg-center bg-no-repeat mx-auto"
       [style.backgroundImage]="
         'url(' + appDataStore.homeData().heroImageLink + ')'
       "
-      [style.width.px]="appDataStore.homeData().heroImageWidth"
-      [style.height.px]="appDataStore.homeData().heroImageHeight"
     >
       <div
         class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
