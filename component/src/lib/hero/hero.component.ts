077c8d1bbce80fa42563258171808ebe32f3a38b
brettaio <dev@bretta.io>
2025-03-05 16:02:03 -0500

    mobile image for hero and preloaded

--- component/src/lib/hero/hero.component.ts
+++ component/src/lib/hero/hero.component.ts
@@ -7,20 +7,25 @@ import { appDataStore } from '../../../../websites/enzo-concrete/src/app/store/a
   imports: [CtaButtonAltComponent],
   template: `
     <section class="relative mx-auto overflow-hidden">
-      <!-- Add the hero image as an img element -->
-      <img
-        [src]="appDataStore.homeData().heroImageLink"
-        alt="Hero Image"
-        class="absolute inset-0 w-full h-full object-cover"
-        loading="eager"
-      />
+      <picture>
+        <!-- Mobile version for viewports 600px and below -->
+        <source
+          media="(max-width: 525px)"
+          srcset="/brushed-concrete02-256.webp"
+        />
+        <!-- Desktop fallback image -->
+        <img
+          alt="Hero Image"
+          loading="eager"
+          class="absolute inset-0 w-full h-full object-cover"
+          src="/brushed-concrete-hero.webp"
+        />
+      </picture>
 
-      <!-- Optional: Overlay gradient -->
       <div
         class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
       ></div>
 
-      <!-- Hero content -->
       <div
         class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
       >
@@ -33,7 +38,7 @@ import { appDataStore } from '../../../../websites/enzo-concrete/src/app/store/a
             class="mt-4 max-w-lg sm:text-xl/relaxed"
             [innerHTML]="appDataStore.homeData().heroParagraph"
           ></div>
-          <div class="m-12 inline-block xs:hidden">
+          <div class="inline-block xs:hidden">
             <component-cta-button-alt
               customClasses="text-lg font-bold text-slate-600"
             />
