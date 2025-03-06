4948b46c937cdec55baab495022ab2986b576473
brettaio <dev@bretta.io>
2025-03-06 04:00:29 -0500

    header black rectangle look

--- component/src/lib/problem-proposition/problem-proposition.component.ts
+++ component/src/lib/problem-proposition/problem-proposition.component.ts
@@ -15,7 +15,7 @@ import { HelmetSafetyIcon } from '../../../../icon/src/lib/helmet-safety/helmet-
     HelmetSafetyIcon,
   ],
   template: `
-    <div class="bg-white py-24 sm:py-32">
+    <div class="bg-slate-200 py-24 sm:py-32">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:text-center">
           <div
